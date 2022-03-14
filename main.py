1fb3738ddf4e6fe4c457f9a00e94304d72fbbdbf
Arhum Jain <arhumjain10@gmail.com>
2022-03-14 07:52:12 -0700

    py updates

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import random
 import colorama
+from collections import defaultdict
+
+
+guesses = open("valid_guesses.csv", "r").read().split("\n")
+solutions = open("valid_solutions.csv", "r").read().split("\n")
 
 
 def printc(colorCode="", text="", endline="\n"):
@@ -10,25 +15,73 @@ def clear():
     if os.name == "nt": os.system('cls')
     else: os.system('clear')
 
+def bestGuess(answer, guess, infoDist):
+    for word in (guesses + solutions):
+        for i in range(0,len(answer)):
+            if 
 def play():
-    def displayGuess(answer, guess):
+    def getInfoDistribution(answer, guess):
+        dist = [0] * len(answer)
+        answerFreq = defaultdict(lambda: 0, { i : answer.count(i) for i in answer })
         for i in range(0, len(answer)):
-            if answer[i] == guess[i]: 
+            if guess[i] == answer[i]:
+                answerFreq[guess[i]] -= 1
+                dist[i] = 2
+        for i in range(0, len(answer)):
+            if answerFreq[guess[i]] != 0:
+                dist[i] = 1
+                answerFreq[guess[i]] -= 1
+            elif dist[i] != 2:
+                dist[i] = 0
+        return dist
+    def displayGuess(answer, guess):
+        infoDist = getInfoDistribution(answer, guess)
+        for i in range(0, len(answer)):
+            if infoDist[i] == 2:
                 printc("\u001b[32m", guess[i], endline=" ")
-            elif guess[i] in answer[i]:
+            elif infoDist[i] == 1:
                 printc("\u001b[33m", guess[i], endline=" ")
             else:
-                printc("", guess[i], endline="")
+                printc("", guess[i], endline=" ")
         print("\n")
 
-    guesses = open("valid_guesses.csv", "r").read().split("\n")
-    solutions = open("valid_solutions.csv", "r").read().split("\n")
     
     answer = solutions[random.randrange(len(solutions))]
     guessCount = 0
     madeGuesses = []
 
-    for i in range(0, guessCount):
+    guess = ""
+
+    while (True):
+        clear()
+        printc("\u001b[31m", answer)
+        for i in range(0, guessCount):
+            print(f"{i+1}. ", end="")
+            displayGuess(answer, madeGuesses[i])
+        printc("\u001b[37m", "-------------------------------")
+        if guess == answer:
+            printc("\u001b[32m", "Amazing, ", endline="")
+            input("you got the right answer! Press Enter to continue...")
+            return
+        elif guessCount == 6:
+            printc("\u001b[31m", "Out of guesses, ", endline="")
+            input(f"the correct answer was: '{answer}', press Enter to continue...")
+            return
+        else:
+            guess = input("Enter a guess, enter in 'bot' to find the next best word, or enter 'q' to quit: ").lower()
+            if guess == "bot":
+                guessCount += 1
+                madeGuesses.append(bestGuess(answer, guess, getInfoDistribution(answer, guess)))
+            elif guess == "q":
+                return
+            elif len(guess) != 5: 
+                input("Guess must be a 5 letter word, press Enter to continue...")
+            elif guess not in guesses and guess not in solutions:
+                input("Guess is not a valid word, press Enter to continue...")
+            else:
+                guessCount += 1
+                madeGuesses.append(guess)
+            
 
 
 clear()
@@ -36,14 +89,16 @@ colorama.init()
 
 run = True
 
-
-
 while (run):
     option = ""
-    while (option != "1" or option != "2"): 
-        clear()
-        printc("\u001b[33m", "Wordle Sight")
-        print("1. Play Wordle!")
-        print("2. Run Bot")
-        option = input("Choose an option: ")
+    clear()
+    printc("\u001b[33m", "Wordle Sight")
+    print("1. Play Wordle!")
+    print("2. Run Bot")
+    option = input("Choose an option: ")
     if (option == "1"):
+        play()
+    elif (option == "2"):
+        pass
+    else:
+        input("That is not a valid option, press Enter to continue...")
